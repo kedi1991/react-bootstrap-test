63366c99f5e453e17b38932c8959be6099f4e411
OKURUT Henry Kedi <hokurut@netcompany.com>
2024-01-06 03:53:48 +0100

    Add form submission and change of state

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 const MyForm = () => {
+
+    //functions to setstate and update the inputs
+    const [state, setState] = useState({
+        user: {
+            email: "",
+            password: ""
+        }
+    })
+
+    const updateInput = (e) => {
+        setState({
+            state, user: {
+                ...state.user, [e.target.name]: e.target.value
+            }
+        })
+
+    }
+
+    const submitter = () => {
+        console.log(state)
+    }
+
     return (
         <Container>
             <Row>
@@ -9,13 +31,14 @@ const MyForm = () => {
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="email address" />
+                            <Form.Control type="email" name={"email"} onChange={updateInput}
+                                          placeholder="email address"/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="email" placeholder="password" />
+                            <Form.Control type="password" name={"password"} onChange={updateInput} placeholder="password"/>
                         </Form.Group>
-                        <Button variant="primary" type={"submit"}>Primary</Button>
+                        <Button variant="primary" onClick={submitter} type={"submit"}>Primary</Button>
                     </Form>
                 </Col>
                 <Col></Col>
@@ -23,5 +46,7 @@ const MyForm = () => {
 
         </Container>
     );
+
 };
+
 export default MyForm;
